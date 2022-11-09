bea021ea2413b59276301a4395edae02dfa56b9a
sameer <sameersahu200138@gmail.com>
2022-11-09 13:54:20 +0530

    Completed Navbar Section

--- src/navbar.js
+++ src/navbar.js
@@ -1,44 +1,77 @@
 import {FaHome} from 'react-icons/fa'
+import {HiMenuAlt3 } from 'react-icons/hi'
+import { useRef } from "react"
+import userEvent from '@testing-library/user-event';
 
 function Navbar() {
-    return ( 
-        <nav className= 'flex items-center text-lg py-3 justify-between m-auto'>
 
-  
+    const btnRef  = useRef()
 
+    function showMenu(){
+            if(btnRef.current.className === 'hidden'){
+                btnRef.current.className = ""
+            }
+            else{
+                btnRef.current.className = 'hidden'
+            }
+
+    }
+
+    return ( <>
+        <nav className= 'flex items-center text-lg py-3 justify-between m-auto pr-3'>
             <div className='flex gap-1 text-navlogocolor '>
-            <FaHome className='m-auto text-2xl '/>
-            <h1 className=' pr-8 text-xl m-auto '>Estatery</h1>
+            <FaHome className='m-auto text-3xl '/>
+            <h1 className=' pr-8 text-2xl m-auto  '>Estatery</h1>
 
-            <ul className='list-none flex gap-5 '>
-                    <li className='  text-black border-2 border-black rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'>Rest</li>
-                    <li className=' ext-black border-2 border-black rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'>Buy</li>
-                    <li className='ext-black border-2 border-black rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'>Sell</li>
-
-                    <li className='ext-black border-2 border-black rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'>Manage Property </li>
-                    <li className='ext-black border-2 border-black rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'> Resources </li>
-
-                </ul>
             </div>
-         
-           
 
-            <div>
-       
+
+            <div className='hidden lg:flex'>     
+            <ul className='list-none flex gap-5 m-auto'>
+                    <a href=""><li className='  text-black  rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'>Rest</li></a>
+                    <a href=""><li className=' text-black  rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'>Buy</li></a>
+                    <a href=""><li className='text-black  rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'>Sell</li></a>
+
+                    <a href=""><li className='text-black  rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white whitespace-nowrap'>Manage Property </li></a>
+                    <a href=""><li className='text-black  rounded px-2 py-1 hover:bg-navlinkcolor hover:text-white'> Resources </li></a>
+
+            </ul>
             </div>
             
-
-            
-
-            <div>
-                <ul className='list-none flex gap-2 '>
-                <li className=' text-loginbuttoncolor bg-white rounded px-2 py-1b border-2 border-black ' >Login</li>
-                    <li className=' bg-loginbuttoncolor text-white rounded px-2 py-1'>SignUp</li>
-                    
-
+            <div className='hidden lg:flex'>
+                <ul className='list-none flex gap-2 m-auto'>
+                <li className=' bg-loginbuttoncolor text-white rounded px-2 py-1 hover:text-yellow-300 ' > SignUp </li>
                 </ul>
             </div>
+
+
+            {/* Mobile button */}
+            <div className='lg:hidden flex items-center text-2xl'>
+                <button onClick={() => showMenu()}>
+                <HiMenuAlt3/>
+                </button>
+
+            </div>
+
         </nav>
+
+        
+            {/* Mobile Menu */}
+            <div ref = {btnRef} className='hidden '>
+                <div className='flex-col '>
+                <a href="" className='text-black  rounded text-center py-3 hover:bg-navlinkcolor hover:text-white block'>Rest</a>
+                    <a href="" className='text-black  rounded text-center py-3 hover:bg-navlinkcolor hover:text-white block'>Buys</a>
+                    <a href="" className='text-black  rounded text-center py-3 hover:bg-navlinkcolor hover:text-white block'>Sell</a>
+                    <a href="" className='text-black  rounded text-center py-3 hover:bg-navlinkcolor hover:text-white block'>Manage Property </a>
+                    <a href="" className='text-black  rounded text-center py-3 hover:bg-navlinkcolor hover:text-white block'>Resources</a>
+
+    
+                    <a href="#" className='text-black  rounded text-center py-3 hover:bg-navlinkcolor hover:text-white block'> Login </a> 
+                    <a href="#" className='text-black  rounded text-center py-3 hover:bg-navlinkcolor hover:text-white block'>SignUp</a>
+                  
+                    </div>
+            </div>
+            </>
      );
 }
 
